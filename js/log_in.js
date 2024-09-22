f5c0abed9413258279d29ad942080a0f741ddaf5
Libertad <lrivascedillo@gmail.com>
2024-09-21 22:33:04 -0600

    Email and password validations were implemented on the login page.

--- js/log_in.js
+++ js/log_in.js
@@ -1,3 +1,4 @@
+// elementos DOM
 const btnLogin = document.getElementById("btnLogIn");
 
 const txtEmail = document.getElementById("correo-ipt")
@@ -5,11 +6,10 @@ const txtPassword = document.getElementById("password-ipt");
 
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
-//función RegExp
-const emailRegex = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
 //bandera
 let isValid = true;
 
+//Validaciones al dar click en botón "Iniciar sesión"
 btnLogin.addEventListener("click", function (event) {
     event.preventDefault();
     txtEmail.style.border = "";
@@ -17,29 +17,57 @@ btnLogin.addEventListener("click", function (event) {
     alertValidaciones.style.display = "none";
     alertValidacionesTexto.innerHTML = "";
     isValid = true;
-    //Validación Email -- RegExp
-    if (!emailRegex.test(txtEmail.value)) {
+
+    //Email vacío
+    if (txtEmail.value.trim() === "") {
         txtEmail.style.border = "solid red medium";
-        alertValidacionesTexto.innerHTML += "El <strong>CORREO</strong> no es correcto <br>";
+        alertValidacionesTexto.innerHTML += "El <strong>CORREO</strong> no puede estar vacío<br>";
         alertValidaciones.style.display = "block";
         isValid = false;
-    } //cierre validación Email
 
-    //validación contraseña
-    if (txtPassword.value.length < 8) {
-        txtPassword.style.border = "solid red medium"
-        alertValidacionesTexto.innerHTML += "La <strong>CONTRASEÑA</strong> no es correcta";
+    }//if email vacío
+
+    //Contraseña vacía
+    if (txtPassword.value.trim() === "") {
+        txtPassword.style.border = "solid red medium";
+        alertValidacionesTexto.innerHTML += "La <strong>CONTRASEÑA</strong> no puede estar vacía <br>";
         alertValidaciones.style.display = "block";
         isValid = false;
-    } //cierre validación contraseña
+    }//if password va
 
-
-    //todo correcto
+    //Autenticación 
     if (isValid) {
-        btnLogin.value = "Iniciar sesión";
-        txtEmail.value = "";
-        txtPassword.value = "";
+        const email = txtEmail.value.trim();
+        const password = txtPassword.value.trim();
 
-    }
+        //Validación de usuario en localStorage
+        const usuario = localStorage.getItem(email);
+
+        if (usuario) {
+            //pasar a objeto
+            const usuarioObj = JSON.parse(usuario);
+
+            //verificar contraseña
+            if (usuarioObj.password === password) {
+                alert("Inicio de sesión exitoso"); ///////bootstrap
+                txtEmail.value = "";
+                txtPassword.value = "";
+                ////Redirección a perfil ? 
+
+            } else {
+                txtPassword.style.border = "solid red medium";
+                alertValidacionesTexto.innerHTML += "La <strong>CONTRASEÑA</strong> es incorrecta";
+                alertValidaciones.style.display = "block";
+            }//else password !===
+
+
+        } else {
+            txtEmail.style.border = "solid red medium";
+            alertValidacionesTexto.innerHTML += "<strong>CORREO</strong> no encontrado";
+            alertValidaciones.style.display = "block";
+        }//else usuario existente
+
+    }//if isValid
+
 
 }); //cierre btnLogin

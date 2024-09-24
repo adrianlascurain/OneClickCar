0dcef1f1844a8cf9e57296fed5eddf1f41570f10
Antonio Rosas <lant8266@gmail.com>
2024-09-24 17:24:36 -0600

    Se sube página Sign In del proyecto. Versión preeliminar.

---
+++ js/Main.js
@@ -0,0 +1,85 @@
+
+// Expresión regular para validar la contraseña
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;  // Al menos 8 caracteres, una letra y un número.
+// Expresión regular para validar el teléfono (formato sencillo: solo dígitos, entre 7 y 15 caracteres)
+const phoneRegex = /^\d{7,15}$/;
+// Expresión regular para validar el nombre (solo letras, con un mínimo de 2 caracteres)
+const nameRegex = /^[A-Za-z\s]{2,}$/;
+// Expresión regular para validar el correo electrónico
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+document.getElementById('submit').addEventListener('click', function (e) {
+    e.preventDefault();  // Evita que el formulario se envíe automáticamente
+
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('inputPassword2').value;
+    const confirmPassword = document.getElementById('inputPassword6').value;
+
+    // Validar nombre
+    if (!nameRegex.test(name)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Nombre inválido',
+            text: 'El nombre debe contener al menos 2 letras y solo puede incluir letras.',
+        });
+        return;
+    }
+
+    // Validar teléfono
+    if (!phoneRegex.test(phone)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Teléfono inválido',
+            text: 'El teléfono debe contener solo dígitos y tener entre 7 y 15 caracteres.',
+        });
+        return;
+    }
+
+    // Validar correo electrónico
+    if (!emailRegex.test(email)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Correo Electrónico inválido',
+            text: 'Por favor, ingresa un correo electrónico válido.',
+        });
+        return;
+    }
+
+    // Verificar si las contraseñas coinciden
+    if (password !== confirmPassword) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error de contraseña',
+            text: 'Las contraseñas no coinciden.',
+        });
+        return;
+    }
+
+    // Validar contraseña con regex
+    if (!passwordRegex.test(password)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Contraseña inválida',
+            text: 'La contraseña debe tener al menos 8 caracteres, incluir letras y al menos un número.',
+        });
+        return;
+    }
+
+    // Guardar los datos en local storage
+    localStorage.setItem('name', name);
+    localStorage.setItem('phone', phone);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);  
+
+    // Mostrar mensaje de éxito
+    Swal.fire({
+        icon: 'success',
+        title: 'Datos guardados',
+        text: 'Los datos han sido guardados correctamente en local storage.',
+    }).then(() => {
+        // Reiniciar el formulario una vez que el usuario cierra la alerta de éxito
+        document.getElementById('userForm').reset();
+    });
+});
---
+++ js/Navigation.js
@@ -0,0 +1,87 @@
+const nav_bar = document.getElementById("nav_bar");
+const footer_back = document.getElementById("footer_back");
+
+function createNavFoot() {
+  let currentPath = location.href;
+  let htmlContenidoNav = `<nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img class="logo" src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725394895/OneClickCar/logoOneClickCar.png" alt="logo" id="logoOneClickCar">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto options">
+                        <a class="nav-link" id="navIndex" aria-current="page" href="https://adrianlascurain.github.io/OneClickCar/index.html">INICIO</a>
+                        <a class="nav-link" id="navContact" href="https://adrianlascurain.github.io/OneClickCar/pages/contact.html">CONTACTO</a>
+                        <a class="nav-link" id="navAbout" href="https://adrianlascurain.github.io/OneClickCar/pages/about.html">NOSOTROS</a>
+                        <a class="nav-link" id="navLogIn" href="https://adrianlascurain.github.io/OneClickCar/pages/log_in.html">INICIAR SESIÓN</a>
+                        <a class="nav-link" id="navSignIn" href="https://adrianlascurain.github.io/OneClickCar/pages/sign_in.html">REGISTRARSE</a>
+                        <a class="nav-link" id="navUserProfile" href="https://adrianlascurain.github.io/OneClickCar/pages/user_profile.html">PERFIL</a>
+                        <a class="nav-link" id="navProductList" href="https://adrianlascurain.github.io/OneClickCar/pages/product_list.html">PRODUCTOS</a>
+                    </div>
+                </div>
+            </div>
+        </nav>`;
+  nav_bar.insertAdjacentHTML("beforeend", htmlContenidoNav);
+
+  let htmlContenidoFooter = `<h2 height="100vh">Copyright &copy OneClickCar</h2>
+        <div class="footer-container">
+            <div class="contact-info mx-3">
+                <h3>Conócenos</h3>
+                <a href="https://www.instagram.com/OneClickCar/">
+                    <img src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725421052/OneClickCar/n4vgpz2ykdqrejz53uxc.png" alt="Instagram">
+                    https://www.instagram.com/OneClickCar/
+                </a>
+                <br>
+                <br>
+                <a href="https://www.facebook.com/OneClickCar/">
+                    <img src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725421052/OneClickCar/olqffplsdykzjv3iiavd.png" alt="Facebook">
+                    https://www.facebook.com/OneClickCar/
+                </a>
+                <br>
+                <br>
+                <a href="https://www.tiktok.com/OneClickCar/">
+                    <img src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725421052/OneClickCar/ncrvqyml2zngmeeuyfl7.png" alt="TikTok">
+                    https://www.tiktok.com/OneClickCar/
+                </a>
+            </div>
+            <div class="company-info mx-3">
+                <h3>Contáctanos</h3>
+                <p><img src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725421052/OneClickCar/rnnaimpxwzllqjhs97zj.png" alt="ubicacion"><b>Dirección:</b> Av. de los Insurgentes Sur s/n, Benito Juárez, CDMX</p>
+            
+                <p> <img src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725421052/OneClickCar/x658h5jy83qf2bb1as8y.png" alt=""><b>Teléfono:</b> +52 555 555 55 55</p>
+            
+                <p><img src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725421051/OneClickCar/cw4nzzwjirjpf8imni4p.png" alt=""><b>Email:</b> contacto@oneclickcar.com</p>
+            </div>
+            <div class="subscribe-section mx-3 text-center">
+                <h3 class="text-center" id="publicidad">Obtén publicidad de nuestros productos</h3>
+                <form action="#">
+                    <input type="email" placeholder="Tu correo electrónico">
+                    <div class="checkbox-container">
+                        <input type="checkbox" id="consent" name="consent">
+                        <label for="consent">Confirmar para obtener correo</label>
+                    </div>
+                    <button type="submit">Confirmar</button>
+              </form>
+            </div>
+        </div>
+        <img id="OneClickCar" src="https://res.cloudinary.com/dz6zf3yio/image/upload/v1725394895/OneClickCar/logoOneClickCar.png" alt="OneClickCar" width="300px">
+        <p>&copy; 2024 Mi Sitio Web. Todos los derechos reservados.</p>
+        <div class="social-media">
+            <a href="https://adrianlascurain.github.io/OneClickCar/pages/Terminos.html" title="Terminos">Términos del Sitio Web</a>
+            <a>|</a>
+            <a href="https://adrianlascurain.github.io/OneClickCar/pages/Terminos.html" title="Politica">Politica de Privacidad</a>
+            <a>|</a>
+            <a href="https://adrianlascurain.github.io/OneClickCar/pages/Terminos.html" title="Declaración">Declaración de accesibilidad</a>
+            <a>|</a>
+            <a href="https://adrianlascurain.github.io/OneClickCar/pages/Terminos.html" title="Conducta">Codigo de Conducta para Proveedores</a>
+            <a>|</a>
+            <a href="https://adrianlascurain.github.io/OneClickCar/pages/Terminos.html" title="Info">No Vender Mi Información</a>
+        </div>`;
+  footer_back.insertAdjacentHTML("beforeend", htmlContenidoFooter);
+}
+
+createNavFoot();

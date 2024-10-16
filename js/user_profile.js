d280af2a620203c8151bfe96ebb0c5393a6c80b6
sergio-rolon <rolonsergio30@gmail.com>
2024-10-16 17:59:57 -0600

    cambios en user_profile

--- js/user_profile.js
+++ js/user_profile.js
@@ -68,3 +68,93 @@ linkLogOut.addEventListener("click",(event) =>{
       "https://adrianlascurain.github.io/OneClickCar/pages/log_in.html";
   }
 })//logOutBtn click
+
+
+//function validateUser
+function validateUser() {
+	
+  let emailUser = sessionStorage.getItem("user");
+
+ const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer: ${sessionStorage.getItem("token")}`);
+  
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    };
+
+fetch(`http://localhost:8080/api/users/email/${emailUser}`, requestOptions)
+  .then((response) => response.json())
+  .then((userData) => {
+if (userData.typeUser == "admin") {
+  infoCliente.classList.add("d-none");
+  infoCliente.innerHTML = "";
+  
+  createTableAdmFetch();
+
+  
+
+
+  btnAgregarAdm.addEventListener("click", (event) => {
+    event.preventDefault();
+      // Validamos que no haya campos vacíos, si no lanzamos sweet alert
+      if (noSerieTextAdd.value != "" &&
+          tipoTextAdd.value != "" &&
+          marcaTextAdd.value != "" &&
+          nombreTextAdd.value != "" &&
+          anioTextAdd.value != "" &&
+          kilTextAdd.value != "" &&
+          transTextAdd.value != "" &&
+          precioTextAdd.value != "" &&
+          imgTextAdd.value != "" &&
+          duenosTextAdd.value != "" &&
+          descripTextAdd.value != "" &&
+          verifTextAdd.value != "" &&
+          vendTextAdd.value != "" &&
+          idVendedorTextAdd.value != "") {
+        addCarAdmFetch()
+        
+      } else {
+        alertFailure("Registro fallido","Ningún campo puede estar vacío" );
+      }//else
+  });//btnAgregar.addEventListener()
+
+
+  btnChangeCars.addEventListener("click", event => {
+    infoAdmin.innerHTML = "";
+    createTableAdmFetch();
+  });
+  btnChangeTrans.addEventListener("click", event => {
+    infoAdmin.innerHTML = "";
+    createTableAdmFetchTrans();
+  });
+  btnChangeComm.addEventListener("click", event => {
+    infoAdmin.innerHTML = "";
+    createTableAdmFetchComm();
+  });
+
+} else if (userData.typeUser =="client") {
+  // Escondemos la vista de administrador
+  infoAdmin.innerHTML = "";
+  headerAdmin.innerHTML = "";
+  
+  createCarouselSold();
+  //createCarouselBuyed();
+  //------------------------------------------------PENDIENTE
+} else {
+    infoCliente.classList.add("d-none");
+  infoAdmin.classList.add("d-none");
+  if ((window.location.pathname = "/pages/purchase_history.html")) {
+    // local
+    window.location.href = "../pages/sign_in.html";
+  } else {
+    // github
+    window.location.href =
+      "https://adrianlascurain.github.io/OneClickCar/pages/sign_in.html";
+  }
+}
+  }).catch((error) => console.error(error));
+}// function validateUser
+// ***********Ejecución
+validateUser();

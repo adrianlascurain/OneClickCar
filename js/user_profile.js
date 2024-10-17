11b9673b257f0710643c1b91e0c933be69e2d3d3
sergio-rolon <rolonsergio30@gmail.com>
2024-10-17 00:00:59 -0600

    change23

--- js/user_profile.js
+++ js/user_profile.js
@@ -1,7 +1,7 @@
 let navUserProfile = document.getElementById("navUserProfile").classList.add("active");
 
 let infoCliente = document.getElementById("infoCliente");
-let infoAdmin = document.getElementById("infoAdmin");
+// let infoAdmin = document.getElementById("infoAdmin");
 let headerAdmin = document.getElementById("headerAdmin");
 let linkProfile = document.getElementById("linkProfile");
 let linkPayment = document.getElementById("linkPayment");
@@ -9,6 +9,17 @@ let linkPurchase = document.getElementById("linkPurchase");
 let linkLogOut = document.getElementById("linkLogOut");
 let linkDeposit = document.getElementById("linkDeposit");
 
+let fullNameTextNew= document.getElementById("fullName-ipt-new");
+let phoneNumberTextNew = document.getElementById("phoneNumber-ipt-new");
+let emailTextNew = document.getElementById("email-ipt-new");
+let birthDateTextNew = document.getElementById("birthDate-ipt-new");
+let typeUserTextNew= document.getElementById("typeUser-ipt-new");
+
+let fullNameTextAdd= document.getElementById("fullName-ipt-add");
+let phoneNumberTextAdd = document.getElementById("phoneNumber-ipt-add");
+let emailTextAdd = document.getElementById("email-ipt-add");
+let birthDateTextAdd = document.getElementById("birthDate-ipt-add");
+let typeUserTextAdd= document.getElementById("typeUser-ipt-add");
 
   linkProfile.addEventListener("click", (event) => {
     event.preventDefault();
@@ -72,6 +83,128 @@ linkLogOut.addEventListener("click",(event) =>{
   }
 })//logOutBtn click
 
+// Crear tabla para dar dinamismo a los datos mostrados
+function createTableAdmFetch() {
+   const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer: ${sessionStorage.getItem("token")}`); 
+  const requestOptions = {
+     method: "GET",
+     headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:8080/api/users/", requestOptions)
+    .then((response) => response.json())
+    .then((dataUsers) => {
+  infoAdmin.innerHTML = "";
+  let htmlContent = "";
+  htmlContent += ` 
+
+  <div class="text-left">
+  <a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAgregar">Agregar</a></div>
+  <div class="table-responsive">
+  <table class="table table-bordered  border-secondary align-middle"> 
+      <thead>
+        <tr>
+      <th scope="col">Id user</th>
+      <th scope="col">Nombre completo</th>
+      <th scope="col">Teléfono</th>
+      <th scope="col">Correo electrónico</th>
+      <th scope="col">Fecha de nacimiento</th>
+      <th scope="col">Tipo de usuario</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+`;
+  for (i = 0; i < dataCarsGeneral.length; i++) {
+    htmlContent += `
+      <tr>
+      <th scope="row">${dataCarsGeneral[i].idCar}</th>
+      <td>${dataUsers[i].idUser}</td> 
+      <td>${dataUsers[i].fullName}</td>
+      <td>${dataUsers[i].phoneNumber}</td>
+      <td>${dataUsers[i].email}</td>
+      <td>${dataUsers[i].birthDate}</td>
+      <td>${dataUsers[i].typeUser}</td>
+      <td><button type="button" class="btn btn-primary" onclick="modificarInfo(${dataUsers[i].idUser})" data-bs-toggle="modal" data-bs-target="#modalModificar">Modificar</button></td>
+      <td><button type="button" class="btn btn-primary" id="btn-modificar-adm" onclick="eliminarInfo(${dataUsers[i].idUser})">Eliminar</button></td>
+    </tr>
+    `;
+  }
+  htmlContent += `</tbody></table></div>`;
+      infoAdmin.insertAdjacentHTML("beforeend", htmlContent);
+      })
+    .catch((error) => console.error(error));
+}//function createTableAdmFetch()
+
+function modUserAdmFetch(idUser) {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer: ${sessionStorage.getItem("token")}`);
+  const requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(
+        `http://localhost:8080/api/users/${idUser}?fullName=${fullNameTextNew.value}&phoneNumber=${phoneNumberTextNew.value}&email=${emailTextNew.value}&birthDate=${birthDateNew.value}&=typeUser${typeUserTextNew.value}`,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((dataUser) => {
+      
+      if (dataUser.length > 0) {
+          fullNameTextAdd.value="";
+          phoneNumberTextAdd.value="";
+          emailTextAdd.value="";
+          birthDateTextAdd.value="";
+          typeUserTextAdd.value="";
+          createTableAdmFetch();
+        // Mostramos mensaje de éxito
+        alertSuccess("Modificación exitosa.", "El usuario fue modificado.");
+      } else {
+        // Mostrar mensaje error
+        alertFailure("Modificación fallida",`Revisa los datos ingresados`);
+      }
+      
+    })
+    .catch((error) => console.error(error));
+}
+
+
+
+
+  function modificarInfo(idUser) {
+    showInfoModAdmFetch(idUser);
+  // Evento modificar, se valida si existe ya un registro, siendo así sí se puede
+  // editar o eliminar, pero no agregar
+  btnModificarAdm.addEventListener("click", (event) => {
+    event.preventDefault();
+    modUserAdmFetch(idUser)
+  });//btnModificarAdm.addEventListener
+  }
+function showInfoModAdmFetch(idUser) {
+    const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer: ${sessionStorage.getItem("token")}`); 
+  const requestOptions = {
+     method: "GET",
+     headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:8080/api/users/", requestOptions)
+    .then((response) => response.json())
+    .then((dataUsers) => {
+     
+    idUserTextNew.setAttribute("disabled","true");
+    fullNameTextNew.value = dataUsers.fullName;
+    phoneNumberTextNew.value = dataUsers.phoneNumber;
+    emailTextNew.value = dataUsers.email;
+    birthDateTextNew.value = dataUsers.birthDate;
+    typeUserTextNew.value = dataUsers.typeUser;
+    })
+    .catch((error) => console.error(error));
+} //showInfoModAdmFetch
 
 //function validateUser
 function validateUser() {
@@ -94,7 +227,7 @@ if (userData.typeUser == "admin") {
   infoCliente.classList.add("d-none");
   infoCliente.innerHTML = "";
   
-  //  createTableAdmFetch();
+  createTableAdmFetch();
 
   btnAgregarAdm.addEventListener("click", (event) => {
     event.preventDefault();
@@ -121,22 +254,10 @@ if (userData.typeUser == "admin") {
   });//btnAgregar.addEventListener()
 
 
-  btnChangeCars.addEventListener("click", event => {
-    infoAdmin.innerHTML = "";
-    createTableAdmFetch();
-  });
-  btnChangeTrans.addEventListener("click", event => {
-    infoAdmin.innerHTML = "";
-    createTableAdmFetchTrans();
-  });
-  btnChangeComm.addEventListener("click", event => {
-    infoAdmin.innerHTML = "";
-    createTableAdmFetchComm();
-  });
 
 } else if (userData.typeUser =="client") {
   // Escondemos la vista de administrador
-  infoAdmin.innerHTML = "";
+  // infoAdmin.innerHTML = "";
   headerAdmin.innerHTML = "";
   console.log(userData);
   let nameUserTxt = document.getElementById("name-ipt");
@@ -152,7 +273,7 @@ if (userData.typeUser == "admin") {
   //------------------------------------------------PENDIENTE
 } else {
     infoCliente.classList.add("d-none");
-  infoAdmin.classList.add("d-none");
+  headerAdmin.classList.add("d-none");
   if ((window.location.pathname = "/pages/purchase_history.html")) {
     // local
     window.location.href = "../pages/sign_in.html";

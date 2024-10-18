857d05db34e64a046b460766f4bc1ce1fd8086d9
sergio-rolon <rolonsergio30@gmail.com>
2024-10-17 23:37:45 -0600

    change

--- js/bank_card.js
+++ js/bank_card.js
@@ -1,3 +1,11 @@
+// Items de menú izquierdo
+let linkProfile = document.getElementById("linkProfile");
+let linkPayment = document.getElementById("linkPayment");
+let linkPurchase = document.getElementById("linkPurchase");
+let linkLogOut = document.getElementById("linkLogOut");
+let linkDeposit = document.getElementById("linkDeposit");//Deposit
+
+
 const cardsImages = {
   bbva:"https://res.cloudinary.com/darjvaffy/image/upload/v1728622014/tarjeta_bbva_hrajq7.jpg",
   banorte: "https://res.cloudinary.com/darjvaffy/image/upload/v1728626782/banorte_ty3rmr.jpg",
@@ -555,3 +563,66 @@ function getCards () {
 .catch((error) => console.error(error));
 }
 getCards();
+
+// ------------------------ Elementos y eventos de la barra izquierda
+linkProfile.addEventListener("click", (event) => {
+  event.preventDefault();
+  if ((window.location.pathname = "/pages/user_profile.html")) {
+    // local
+    window.location.href = "../pages/user_profile.html";
+  } else {
+    // github
+    window.location.href =
+      "https://adrianlascurain.github.io/OneClickCar/pages/user_profile.html";
+  }
+});
+
+linkPurchase.addEventListener("click", (event) => {
+  event.preventDefault();
+  if ((window.location.pathname = "/pages/purchase_history.html")) {
+    // local
+    window.location.href = "../pages/purchase_history.html";
+  } else {
+    // github
+    window.location.href =
+      "https://adrianlascurain.github.io/OneClickCar/pages/purchase_history.html";
+  }
+});
+
+linkPayment.addEventListener("click", (event) => {
+  event.preventDefault();
+  if ((window.location.pathname = "/pages/payment_account.html")) {
+    // local
+    window.location.href = "../pages/payment_account.html";
+  } else {
+    // github
+    window.location.href =
+      "https://adrianlascurain.github.io/OneClickCar/pages/payment_method.html";
+  }
+});
+
+linkDeposit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if ((window.location.pathname = "/pages/deposit_account.html")) {
+    // local
+    window.location.href = "../pages/deposit_account.html";
+  } else {
+    // github
+    window.location.href =
+      "https://adrianlascurain.github.io/OneClickCar/pages/deposit_account.html";
+  }
+});
+
+// Clean session storage and go to login page
+linkLogOut.addEventListener("click", (event) => {
+  event.preventDefault();
+  sessionStorage.clear();
+  if ((window.location.pathname = "/pages/user_profile.html")) {
+    // local
+    window.location.href = "../pages/log_in.html";
+  } else {
+    // github
+    window.location.href =
+      "https://adrianlascurain.github.io/OneClickCar/pages/log_in.html";
+  }
+}); //logOutBtn click
